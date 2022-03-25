542621a95978e612dfe243914cb42d5168a4d7db
Rezuan Ahmed <zikzack06@gmail.com>
2022-03-25 19:03:52 +0600

    Done Perfectly

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+const pollController = require("./pollController");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+// Middleware setup
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/create", pollController.createPollGetController);
+app.post("/create", pollController.createPollPostController);
+
+app.get("/polls/:id", pollController.viewPollGetController);
+app.post("/polls/:id", pollController.viewPollPostController);
+app.get("/polls", pollController.getAllPolls);
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+mongoose.connect("mongodb://localhost:27017/express-crash-poll");
+
+app.listen(3000, () => {
+  console.log("Server is running on PORT 3000...");
+});
